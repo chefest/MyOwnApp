9910b4959c9da24d498e6c6f8193322e0e32798a
ctorres <ctorres.developer@gmail.com>
2024-04-21 02:02:21 -0600

    AuthScreen updated.
    The component was updated to use STATE as parameter
    and also callbacks for the events

--- auth/src/main/java/com/chefsito/myownapp/auth/presentation/AuthScreen.kt
+++ auth/src/main/java/com/chefsito/myownapp/auth/presentation/AuthScreen.kt
@@ -5,22 +5,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.chefsito.myownapp.common.components.InputText
 import com.chefsito.myownapp.common.components.Separator
 import com.chefsito.myownapp.common.components.SubmitButton
 import com.chefsito.myownapp.auth.R
+import com.chefsito.myownapp.common.components.OnClickSubmit
+import com.chefsito.myownapp.common.components.OnValueChange
 
 
 @Composable
 fun AuthScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    state: AuthScreenState = AuthScreenState(),
+    onUsernameChanged: OnValueChange = {},
+    onPasswordChanged: OnValueChange = {},
+    onClickSubmit: OnClickSubmit = {}
 ) {
     Column(
         modifier = modifier
@@ -32,28 +36,22 @@ fun AuthScreen(
     ) {
         InputText(
             modifier = modifier.fillMaxWidth(),
-            value = "",
+            value = state.username,
             label = stringResource(id = R.string.username_email),
-            onValueChange = {
-                println("on value change")
-            }
+            onValueChange = onUsernameChanged
         )
         InputText(
             modifier = modifier.fillMaxWidth(),
-            value = "",
+            value = state.password,
             label = stringResource(id = R.string.password),
-            onValueChange = {
-                println("on value change")
-            },
+            onValueChange = onPasswordChanged,
             isPassword = true
         )
         Separator(value = 10.dp)
         SubmitButton(
             modifier = modifier.fillMaxWidth(),
             value = "Enter",
-            onClickSubmit = {
-                println("Enter clicked")
-            }
+            onClickSubmit = onClickSubmit
         )
     }
 }
