393c99f8b4348454de30c5a5f3a1b38526558514
ctorres <ctorres.developer@gmail.com>
2024-04-21 02:04:02 -0600

    The viewmodel for AuthScreen implements Hilt.
    StateFlow added for the screen values
    Event functions added used in the auth screen

--- auth/src/main/java/com/chefsito/myownapp/auth/presentation/AuthScreenViewModel.kt
+++ auth/src/main/java/com/chefsito/myownapp/auth/presentation/AuthScreenViewModel.kt
@@ -4,36 +4,68 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.chefsito.myownapp.auth.domain.AuthDomainException
 import com.chefsito.myownapp.auth.domain.AuthUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
-class AuthScreenViewModel : ViewModel() {
+@HiltViewModel
+class AuthScreenViewModel @Inject constructor(
+    val authUseCase: AuthUseCase
+) : ViewModel() {
 
-    fun login(
-        username: String,
-        password: String,
-    ) {
+    /**
+     * This variable will be used to write/read operations in the viewmodel
+     */
+    private val _state: MutableStateFlow<AuthScreenState> = MutableStateFlow(
+        AuthScreenState()
+    )
+
+    /**
+     * This variable will be used to read the information outside from viewmodel
+     */
+    val state: StateFlow<AuthScreenState> = _state.asStateFlow()
+
+    fun onUsernameChanged(value: String) {
+        _state.update { current->
+            current.copy(username = value)
+        }
+    }
+
+    fun onPasswordChanged(value: String) {
+        _state.update { current->
+            current.copy(password = value)
+        }
+    }
+    fun onSubmit() {
+        login()
+    }
+
+    private fun login() {
         try {
-            val authUseCase: AuthUseCase = AuthUseCase()
             authUseCase.exec(
-                username = username,
-                password = password
+                username = this._state.value.username,
+                password = this._state.value.password
             )
         } catch (ex: Exception) {
             when (ex) {
                 AuthDomainException.EmptyLoginFormException -> {
-                    Log.e("error",ex.message, ex)
+                    Log.e("error", ex.message, ex)
                 }
 
                 AuthDomainException.EmptyUserNameException -> {
-                    Log.e("error",ex.message, ex)
+                    Log.e("error", ex.message, ex)
 
                 }
 
                 AuthDomainException.EmptyPasswordException -> {
-                    Log.e("error",ex.message, ex)
+                    Log.e("error", ex.message, ex)
                 }
 
                 AuthDomainException.LengthPasswordException -> {
-                    Log.e("error",ex.message, ex)
+                    Log.e("error", ex.message, ex)
                 }
             }
         }
