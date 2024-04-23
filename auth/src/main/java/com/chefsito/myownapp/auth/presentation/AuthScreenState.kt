3deaddcaf09dc95703d8cdf9c92667609e8a00dd
ctorres <ctorres.developer@gmail.com>
2024-04-23 12:47:40 -0600

    Set login values

--- auth/src/main/java/com/chefsito/myownapp/auth/presentation/AuthScreenState.kt
+++ auth/src/main/java/com/chefsito/myownapp/auth/presentation/AuthScreenState.kt
@@ -3,8 +3,8 @@ package com.chefsito.myownapp.auth.presentation
 import com.chefsito.myownapp.common.core.ScreenStates
 
 data class AuthScreenState(
-    val username: String = "",
-    val password: String = "",
+    val username: String = "kminchelle",
+    val password: String = "0lelplR",
     val errorMessage: String = "",
     val screenStates: ScreenStates = ScreenStates.INIT
 )
