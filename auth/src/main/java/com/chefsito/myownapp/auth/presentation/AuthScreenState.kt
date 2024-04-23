458cca69c4691682456bcd6b14d73923d8c9d892
ctorres <ctorres.developer@gmail.com>
2024-04-23 11:56:30 -0600

    ENUM screen state added to AuthScreenState
    This enum handle the state of the auth compose screen, the state starts on init

--- auth/src/main/java/com/chefsito/myownapp/auth/presentation/AuthScreenState.kt
+++ auth/src/main/java/com/chefsito/myownapp/auth/presentation/AuthScreenState.kt
@@ -1,7 +1,10 @@
 package com.chefsito.myownapp.auth.presentation
 
+import com.chefsito.myownapp.common.core.ScreenStates
+
 data class AuthScreenState(
     val username: String = "",
     val password: String = "",
-    val errorMessage: String = ""
+    val errorMessage: String = "",
+    val screenStates: ScreenStates = ScreenStates.INIT
 )
