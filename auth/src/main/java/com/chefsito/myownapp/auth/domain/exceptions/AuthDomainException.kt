ac10ca620275d007d70abbf98cd7ff446cd74504
ctorres <ctorres.developer@gmail.com>
2024-04-23 01:08:23 -0600

    New Auth Domain Exceptions added.
    These new Domain exceptions are used to handle different exceptions caught from the data layer.

--- auth/src/main/java/com/chefsito/myownapp/auth/domain/exceptions/AuthDomainException.kt
+++ auth/src/main/java/com/chefsito/myownapp/auth/domain/exceptions/AuthDomainException.kt
@@ -1,4 +1,4 @@
-package com.chefsito.myownapp.auth.domain
+package com.chefsito.myownapp.auth.domain.exceptions
 
 import com.chefsito.myownapp.common.core.DomainException
 
@@ -7,4 +7,10 @@ sealed class AuthDomainException(override val message: String?): DomainException
     object EmptyUserNameException: AuthDomainException("The username is empty")
     object EmptyPasswordException: AuthDomainException("The password is empty")
     object LengthPasswordException: AuthDomainException("The length of password is not correct")
+    object FailAPIException: AuthDomainException("There was an error calling the auth api")
+    object NoBodyException: AuthDomainException("The response from the server is empty")
+    object UnknownException: AuthDomainException("Unkown exception")
+
+
+
 }
