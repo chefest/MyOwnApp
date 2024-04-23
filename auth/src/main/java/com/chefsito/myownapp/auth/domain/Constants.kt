b8c6303a2f743ab786777c785d5e7299ca361587
ctorres <ctorres.developer@gmail.com>
2024-04-23 00:59:19 -0600

    Moved to a new Package

--- auth/src/main/java/com/chefsito/myownapp/auth/domain/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.chefsito.myownapp.auth.domain
-
-object Constants {
-    const val PASSWORD_MINIMUM_LENGTH = 8
-}
---
+++ auth/src/main/java/com/chefsito/myownapp/auth/domain/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.chefsito.myownapp.auth.domain.utils
+
+object Constants {
+    const val PASSWORD_MINIMUM_LENGTH = 6
+}
