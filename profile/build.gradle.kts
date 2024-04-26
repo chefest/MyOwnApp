9e542297cd6ff3e39097b67b3945a2c7b3fc5869
ctorres <ctorres.developer@gmail.com>
2024-04-26 14:23:41 -0600

    Module Profile added.

--- profile/build.gradle.kts
+++ profile/build.gradle.kts
@@ -1,25 +1,19 @@
 plugins {
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.hilt)
     kotlin("kapt")
 }
 
 android {
-    namespace = "com.chefsito.myownapp"
+    namespace = "com.chefsito.myownapp.profile"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.chefsito.myownapp"
         minSdk = 28
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -53,6 +47,8 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -61,10 +57,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(project(":auth"))
-    implementation(project(":dashboard"))
     implementation(project(":common"))
-    implementation(project(":profile"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -77,8 +70,6 @@ dependencies {
     implementation(libs.hilt)
     kapt(libs.hiltCompiler)
 
-    //NAVIGATION
-    implementation(libs.navigationUI)
-    implementation(libs.navigationCompose)
-    implementation(libs.hiltNavigation)
+    //RETROFIT
+    implementation(libs.retrofit)
 }
