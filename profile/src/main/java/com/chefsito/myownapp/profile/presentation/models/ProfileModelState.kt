9b8d897295867029f1b9c2254a9c0bd1881d2643
ctorres <ctorres.developer@gmail.com>
2024-04-26 14:27:41 -0600

    Profile Model for the UI State added

---
+++ profile/src/main/java/com/chefsito/myownapp/profile/presentation/models/ProfileModelState.kt
@@ -0,0 +1,8 @@
+package com.chefsito.myownapp.profile.presentation.models
+
+data class ProfileModelState(
+    val name: String = "",
+    val lastName: String = "",
+    val email: String = "",
+    val picture: String = "",
+)
