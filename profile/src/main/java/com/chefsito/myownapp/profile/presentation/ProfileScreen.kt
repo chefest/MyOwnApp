5f4db57aeb4ce0268b7b921056082ff344cf8fb6
ctorres <ctorres.developer@gmail.com>
2024-04-26 14:49:21 -0600

    Picture Component added to Profile Screen

--- profile/src/main/java/com/chefsito/myownapp/profile/presentation/ProfileScreen.kt
+++ profile/src/main/java/com/chefsito/myownapp/profile/presentation/ProfileScreen.kt
@@ -5,14 +5,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.chefsito.myownapp.common.components.Label
 import com.chefsito.myownapp.profile.presentation.models.ProfileModelState
-
+import com.chefsito.myownapp.common.components.Piture
 
 @Composable
 fun ProfileScreen(
     modifier: Modifier,
     profileScreenStateModel: ProfileModelState
-){
+) {
     Column {
+        Piture(
+            modifier = modifier,
+            url = profileScreenStateModel.picture
+        )
         Label(
             value = profileScreenStateModel.name
         )
